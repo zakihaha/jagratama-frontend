831afbda8754c82a0689d917e50503ae917535db
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2025-05-02 01:02:24 +0700

    feat: refresh token

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { API_V1_BASE_URL } from "./lib/config";
+import { jwtDecode } from "jwt-decode";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -34,9 +35,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           name: user.data.name,
           email: user.data.email,
           token: user.data.token,
+          refreshToken: user.data.refresh_token,
           position: user.data.position,
           role: user.data.role,
-          // image: user.image || null, // Optional: if your API returns an image URL
+          image: user.image
         };
       }
     }),
@@ -50,18 +52,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // and we can add the user information to the token (create a JWT)
       if (user) {
         token.accessToken = (user as any).token; // Store your API token inside the JWT
+        token.refreshToken = (user as any).refreshToken; // Store refresh token
         token.role = (user as any).role; // Store user role
         token.id = (user as any).id; // Store user id
         token.position = (user as any).position; // Store user position
-        // token.image = (user as any).image; // Optional: if your API returns an image URL
+        token.image = (user as any).image;
+
+        if ((user as any).token) {
+          const decoded = jwtDecode((user as any).token);
+          token.accessTokenExp = decoded.exp; // Store expiration time
+        }
       }
 
-      // // check jwt expiration
-      // const isExpired = (token as any).exp < Date.now() / 1000;
-      // if (isExpired) {
-      //   console.log("Token expired, refreshing...");
-      //   token = await refreshAccessToken(token);
-      // }
+      // // check jwt backend expiration
+      const isExpired = (token as any).accessTokenExp < Math.floor(Date.now() / 1000);
+      if (isExpired) {
+        token = await refreshAccessToken(token);
+
+        // update the token expiration time
+        if (token.accessToken) {
+          const decoded = jwtDecode((token as any).accessToken);
+          token.accessTokenExp = decoded.exp; // Store expiration time
+        }
+      }
 
       return token;
     },
@@ -72,8 +85,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         (session as any).accessToken = token.accessToken;
         (session as any).user.role = token.role; // Add user role to session
         (session as any).user.position = token.position; // Add user position to session
+        (session as any).user.image = token.image; // Add user image to session
       }
-      
+
       return session;
     },
   },
@@ -87,7 +101,7 @@ async function refreshAccessToken(token: any) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        refreshToken: token.refreshToken,
+        refresh_token: token.refreshToken,
       }),
     });
 
@@ -97,8 +111,7 @@ async function refreshAccessToken(token: any) {
 
     return {
       ...token,
-      accessToken: refreshed.accessToken,
-      refreshToken: refreshed.refreshToken
+      accessToken: refreshed.data.token,
     };
   } catch (err) {
     console.error("Failed to refresh token", err);
