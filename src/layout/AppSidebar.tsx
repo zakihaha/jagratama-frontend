f2214022affc4bab16301f8b412d09ad7e0453d2
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2025-05-19 13:41:43 +0700

    fix(sidebar): show dashboard for reviewer

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -37,7 +37,7 @@ const navItems: NavItem[] = [
     icon: <LayoutGridIcon className="text-current" />,
     name: "Dashboard",
     path: "/jagratama",
-    roles: ["admin", "approver", "requester"],
+    roles: ["admin", "approver", "reviewer", "requester"],
   },
   {
     icon: <UsersIcon />,
