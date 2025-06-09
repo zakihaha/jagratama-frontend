141e3417e36125e1683b6a4471dac80255b22c48
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2025-06-09 15:51:02 +0700

    fix(sidebar): z-index

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -324,7 +324,7 @@ const AppSidebar: React.FC = () => {
 
   return (
     <aside
-      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
+      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-9999 border-r border-gray-200 
         ${isExpanded || isMobileOpen
           ? "w-[290px]"
           : isHovered
