b7f4937093d232dbdfcd04f0f78a89acbe4dd79e
Zaki Choiruddin <muhzakich@students.amikom.ac.id>
2025-06-14 23:44:18 +0700

    fix(projects): remove image domains setting

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['i.pinimg.com'],
     remotePatterns: [
       {
         protocol: "https",
